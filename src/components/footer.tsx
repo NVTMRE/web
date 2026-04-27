3a666e65c079c40940da6541cf7d3c4af5d27dff
NVTMRE <nvtmre@NVTMRE-ASUS.localdomain>
2026-04-27 05:38:20 +0200

    feat: Create next.js project with shadcn/ui

--- src/components/footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="flex justify-around items-center py-2 bg-muted/50 border-y-[1px] border-border">
-            <p>NVTMRE &copy; 2025</p>
-        </footer>
-    )
-}
--- src/types/modules.ts
+++
@@ -1,9 +0,0 @@
-import { ElementType } from "react";
-
-export type Module = {
-    name: string;
-    href: string;
-    icon: ElementType;
-}
-
-export type Modules = Module[]
